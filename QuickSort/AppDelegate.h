889ae2ca67df5b62582762fe9d9be6ed815c8e04
mac <mac@Thinhdts-MacBook-Pro.local>
2015-07-20 21:41:23 +0700

    Initial Commit

---
+++ QuickSort/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QuickSort
+//
+//  Created by mac on 7/20/15.
+//  Copyright (c) 2015 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QuickSort/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QuickSort
+//
+//  Created by mac on 7/20/15.
+//  Copyright (c) 2015 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
